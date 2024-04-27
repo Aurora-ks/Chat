9011686b2f0e63db5d0377a83225d4c633bd2a47
Aurora <2736133099@qq.com>
2024-04-27 18:43:01 +0800

    Revert "add login logic"
    This reverts commit 2fd61e0530be853ce07e3b837f18aa7b9da9bb02.

--- Client/httpmanager.h
+++ Client/httpmanager.h
@@ -9,7 +9,18 @@
 #include <QJsonObject>
 #include <QJsonDocument>
 #include <memory>
-#include "types.h"
+
+enum RequestID{
+    ID_GET_VARIFY_CODE = 1001, //获取验证码
+    ID_USER_REG = 1002, //用户注册
+    ID_USER_LOGIN = 1003, //用户登录
+};
+
+enum ErrorCodes{
+    SUCCESS = 0,
+    ERR_JSON = 1, //Json解析失败
+    ERR_NETWORK = 2, //网络错误
+};
 
 class HttpManager : public QObject, public std::enable_shared_from_this<HttpManager>
 {
